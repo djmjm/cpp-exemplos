6d4e2bc76f973326b8b03980e2cf27e7d75f36a3
Douglas Janovik Maliszewski <douglasjmjm@gmail.com>
2022-08-09 14:55:45 -0300

    Resolvendo inconsistências na resposta (concorrência).

--- src/wordScramblerThreads/main.cpp
+++ src/wordScramblerThreads/main.cpp
@@ -4,6 +4,7 @@
 #include <mutex>
 #include <time.h>
 #include <chrono>
+#include <mutex>
 
 #define RANDOMIZE srand(time(NULL))
 #define WAIT std::this_thread::sleep_for(std::chrono::milliseconds(x));
@@ -11,39 +12,42 @@
 using namespace std;
 
 string answer = "";
+mutex copy_str;
 
 void get_char_string
 (string origin, int index){
     int x = rand() % 100; WAIT;
-    
+
     while(x--);
-    
-    answer += origin.at(index);
+
+    copy_str.lock();
+        answer += origin.at(index);
+    copy_str.unlock();
 }
 ;
 
 int main(){
-    
+
     RANDOMIZE;
-    
+
     string origin;
-    
+
     cout << "Digite uma palavra ...." << endl;
     cin >> origin;
-    
+
     int num_thread = origin.length();
-    
+
     thread threads[num_thread];
-    
+
     for(int i = 0; i < num_thread; i++){
         threads[i] = thread(get_char_string, origin, i);
     }
-    
+
     for(int i = 0; i < num_thread; i++){
         threads[i].join();
     }
-    
+
     cout << answer << endl;
-    
+
     return 0;
 }
